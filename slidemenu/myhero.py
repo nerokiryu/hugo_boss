cb6908a36c7e7c7fa55b856006d573e623e75889
VIVIANI Nicolas <vivianin@hostname.iut2.upmf-grenoble.fr>
2016-02-01 13:57:46 +0100

    credit

--- slidemenu/myhero.py
+++ slidemenu/myhero.py
@@ -12,6 +12,7 @@ class MyHero(MySprite):
 	def movement(self, width, height):
 		keys = pygame.key.get_pressed()
 		courir = pygame.K_LSHIFT
+		mouvement = 10
 
 
 		# Test la touche pressee
@@ -29,25 +30,25 @@ class MyHero(MySprite):
 
 
 		if keys[courir] and keys[pygame.K_LEFT] and self.rect.left > 0:
-			if self.rect.left < 50:
+			if self.rect.left < mouvement:
 				self.rect = self.rect.move(-self.rect.left, 0)
 			else:
-				self.rect = self.rect.move(-50, 0)
+				self.rect = self.rect.move(-mouvement, 0)
 
 		if keys[courir] and keys[pygame.K_RIGHT] and self.rect.right < width:
-			if self.rect.right > width-50:
+			if self.rect.right > width-mouvement:
 				self.rect = self.rect.move(width-self.rect.right, 0)
 			else:
-				self.rect = self.rect.move(50, 0)
+				self.rect = self.rect.move(mouvement, 0)
 
 		if keys[courir] and keys[pygame.K_UP] and self.rect.top > 0:
-			if self.rect.top < 50:
+			if self.rect.top < mouvement:
 				self.rect = self.rect.move(0, -self.rect.top)
 			else:
-				self.rect = self.rect.move(0, -50)
+				self.rect = self.rect.move(0, -mouvement)
 
 		if keys[courir] and keys[pygame.K_DOWN] and self.rect.bottom < height:
-			if self.rect.bottom > height-50:
+			if self.rect.bottom > height-mouvement:
 				self.rect = self.rect.move(0, height-self.rect.bottom)
 			else:
-				self.rect = self.rect.move(0, 50)
+				self.rect = self.rect.move(0, mouvement)
