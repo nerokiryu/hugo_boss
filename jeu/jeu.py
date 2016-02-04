630015b8645673e6151953465486434d53e8fb7b
BEDONI Corentin <bedonic@hostname.iut2.upmf-grenoble.fr>
2016-02-04 10:22:35 +0100

    Boss 3

--- jeu/jeu.py
+++ jeu/jeu.py
@@ -69,8 +69,10 @@ def main():
     arme.realease()
     if num == 1 :
         boss = Boss(500, 32)
-    else :
+    elif num == 2 :
         boss = Boss2(500, 32)
+    elif num == 3 :
+        boss = Boss3(500, 32)
     boss.realease()
     player = Player(32, 32)
     player.realease()
@@ -148,7 +150,7 @@ def main():
         # update player, draw everything else
         if player.update(up, down, left, right, running, platforms, boss, screen):
             rep = False
-        if boss.update(up, down, left, right, running, platforms, player, arme, screen,total_level_height) and rep == True:
+        if boss.update(up, down, left, right, running, platforms, player, arme, screen,total_level_width,total_level_height) and rep == True:
             rep = False
         if arme.update(up, down, left, right, running, platforms,boss, player, screen) and rep == True:
             rep = False
